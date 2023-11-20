c2f14efe4210b9633dc8792711014ca928c85484
Vadim Kuzminov <vadimsergeevich34@gmail.com>
2023-11-20 22:24:13 +0300

    1

--- Проверка просто числа/Program.cs
+++ Проверка просто числа/Program.cs
@@ -2,15 +2,17 @@
 Console.WriteLine("введите число:");
 int n = int.Parse(Console.ReadLine());
 int i = 2;
-while (i <= n)
+while (i <= n - 1)
 {
     if (n % i == 0)
     {
         prostoe = false;
         Console.WriteLine("число составное");
+
+
         break;
     }
-    else 
+    else
     {
         prostoe = true;
         Console.WriteLine("число простое");
